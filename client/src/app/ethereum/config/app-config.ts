648c93288de004e1eddecee0c6ad2682299b4795
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-06 19:37:54 +0100

    Add ethereum folder for client

---
+++ client/src/app/ethereum/config/app-config.ts
@@ -0,0 +1,17 @@
+export const AppConfig: any = {
+    ethereum: {
+      localNetwork: 'http://192.168.1.71:7545',
+      rinkebyNetwork: 'https://rinkeby.infura.io/v3/82bc3d3749d049248ee3333c6efabc25',
+      privateKey: '6c3c627326a83740209af40831d71afeeee30210dc4dfb5c3096aa9e61f261c2',
+      account: '0x40721976Ca9018A901996D3e10cf351633EDbAE8',
+      recipient: '0xaca88E7B92655F6F7270Cbb18FdD2Ad128C580e4',
+      amount: 0.1
+  
+      /* provider: 'https://rinkeby.infura.io/v3/82bc3d3749d049248ee3333c6efabc25',
+      privateKey: '5d0a44b2f735738d8d121cf8866d45a516582c5dcfacd05e79f431fd3bbe1b98',
+      account: '0xeCDED0f569Ccd0FcEF2bc359e6F742BA1d6e533A',
+      recipient: '0xdedfF08A5afA1669442ba5DEAD3CF723889C6064',
+      amount: 0.1 */
+     
+    }
+  }
