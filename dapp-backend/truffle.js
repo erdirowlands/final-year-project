325d21a1895ee6358db5c2823c89d0c712a428ec
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-06 21:25:16 +0100

    Fix rinkeby truffle config

--- dapp-backend/truffle.js
+++ dapp-backend/truffle.js
@@ -1,6 +1,10 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const privateKey = "5D0A44B2F735738D8D121CF8866D45A516582C5DCFACD05E79F431FD3BBE1B98";
+
 module.exports = {
   networks: {
-
+    
     development: {
       host: "192.168.1.71",
       port: 7545,
@@ -20,15 +24,17 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(privateKey, "https://rinkeby.infura.io/v3/82bc3d3749d049248ee3333c6efabc25");
+      },
+      network_id: '4',
+    },
   },
 
-  rinkeby: {
-    provider: function() {
-      return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/82bc3d3749d049248ee3333c6efabc25");
-    },
-    network_id: '3',
-  },
+  
 
   compilers: {
     solc: {
