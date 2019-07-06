72aa5ba09fb30e7eeb3ef5de256df39fecedf186
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-06 20:47:53 +0100

    Add rinkeby confit to truffle.js

--- dapp-backend/truffle.js
+++ dapp-backend/truffle.js
@@ -1,15 +1,33 @@
 module.exports = {
   networks: {
-    ganacheCLI: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+
+    development: {
+      host: "192.168.1.71",
+      port: 7545,
+      network_id: "*",
+      accounts: 5,
+      defaultEtherBalance: 500,
+      blockTime: 3
     },
+
     ganacheGUI: {
       host: "192.168.1.71",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+
+    ganacheCLI: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
     }
+  },
+
+  rinkeby: {
+    provider: function() {
+      return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/82bc3d3749d049248ee3333c6efabc25");
+    },
+    network_id: '3',
   },
 
   compilers: {
