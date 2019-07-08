f2a3a47be2aefca295436720532e7a881c28c465
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-08 14:27:07 +0100

    marker and mentor note

--- dapp-frontend/src/app/blockchain/provider/web3provider.service.ts
+++ dapp-frontend/src/app/blockchain/provider/web3provider.service.ts
@@ -10,6 +10,10 @@ import { environment } from '../../../environments/environment';
 export class Web3ProviderService {
 
   // Provides a connection to the Ethereum blockchain and contains various utility methods.
+  // For Joe and Fiona: The web3js library has released Typescript definitions, but I've found
+  // their accounts package to have a limited implementation. So, because one of the main
+  // features of this application is to manage user's PPKs, I rely on the complete implementation 
+  // of webjs accounts package and have opted not to use the Typescript definitions.
   public web3: any;
 
 
