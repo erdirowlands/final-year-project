a3cf0eb2d07154dc7ddb24487e6999b7f4a3e693
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-11 18:03:10 +0100

    Add isApprovalStored by flag

--- dapp-backend/contracts/UniversityVoting.sol
+++ dapp-backend/contracts/UniversityVoting.sol
@@ -36,8 +36,8 @@ contract UniversityVoting is Ownable {
 
     mapping(address => ApprovalRequest) _approvalRequestQueue;
 
-    modifier isApproved(address approvedAdminAddress) {
-        require(_approvalRequestQueue[approvedAdminAddress].isPending, "This request has not been approved yet!");
+    modifier hasPendingRequest(address approvedAdminAddress) {
+        require(_approvalRequestQueue[approvedAdminAddress].isPending, "You have an outstanding request, please wait for that to be processed");
         _;
     }
 
@@ -144,6 +144,10 @@ contract UniversityVoting is Ownable {
     
     function isInstitutionAddressStored(address institute) public view returns(bool isStored) {
         return _addressStructMapping[institute].isAddress;
+    }
+
+    function isApprovalStored(address adminAddress) public view returns(bool isStored) {
+        return _approvalRequestQueue[adminAddress].isInitialised;
     }
 
     /**
