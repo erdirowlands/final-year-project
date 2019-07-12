d9ecee449427ce40e24194c53902b5b0f41cbff2
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-12 10:29:29 +0100

    Comments

--- dapp-backend/contracts/Institution.sol
+++ dapp-backend/contracts/Institution.sol
@@ -97,7 +97,8 @@ contract Institution  {
 
     function getAdmin(address storedAdmin) public view returns(string memory, string memory, address, bool) {
         // require(isAdminStored(storedAdmin), "Admin address not found"); // TODO shouldn't need this, as we'll be using the array as the index.
-        if (isAdminStored(storedAdmin)) { // TODO this might not be reachable as the return is in the if if it's anything like Java, test this.
+        if (isAdminStored(storedAdmin)) { // TODO this might not be reachable as the return is in the if if it's anything like Java and
+        //the comment above should apply about using the array as the inex
             return (_institutionAdmins[storedAdmin].firstName, _institutionAdmins[storedAdmin].surname, _institutionAdmins[storedAdmin].adminAddress,
             _institutionAdmins[storedAdmin].isAuthorised);
         }
