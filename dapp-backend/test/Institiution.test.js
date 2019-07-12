20de29c2e04c239c52706f4e84350641c0fd4801
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-12 11:19:27 +0100

    Add test for addingNewAdmin

--- dapp-backend/test/Institiution.test.js
+++ dapp-backend/test/Institiution.test.js
@@ -1,7 +1,7 @@
 const BigNumber = web3.BigNumber;
 
 const UniversityVoting = artifacts.require("UniversityVoting");
-const Institution = artifacts.require('Institution')
+const Institution = artifacts.require("Institution");
 
 require('chai')
 .use(require('chai-bignumber')(BigNumber))
@@ -11,17 +11,18 @@ require('chai')
 contract('Institution', accounts => {
   // UniversityVoting contract is responsible for deploying Institution, so mimick this flow in tests.
   let universityVoting;
+  // The deployed child institution contract address.
   let newInstitutionContractAddress;
   // Me, as the owner and deployer of the contract.
   const developerAccount = accounts[0];
-  // Account for admin who makes a request for a new Institution
+  // Account for admin who makes a request for a new Institution (will become approved in the before hook).
   const prospectiveAdminAccount = accounts[1];
 
   const institutionName = "Ulster University";
   const adminFirstName = "John";
   const adminSurname = "Francis";
 
-  describe('deploy and use the child institution contract', function () {
+  describe('Deploy and use the child institution contract', function () {
     before(async function() {
       universityVoting = await UniversityVoting.new({ from: developerAccount });
       // Submit the approval from 'prospective admin' addresss
@@ -38,16 +39,19 @@ contract('Institution', accounts => {
       );
       // Get emitted event from initialiseInstitutionWithAdmin()
       const log = await result.logs[0].args;
-      // Get newly created contract address from event
-      newInstitutionContractAddress = await log.institution;
+      // Get newly created contract address from event and use truffle-contract to get an instance.
+      newInstitutionContractAddress = await Institution.at(log.institution);
     });
     after(async function() {
       await universityVoting.kill();
     });
 
-    
-    it('creates a new election', async function () {
-    const result = await this.institution.createElection()
+    it('lets an approved institution admin add another admin', async function () {
+      const newAdminFirstName = "Ben";
+      const newAdminSurname = "Sisko"
+      const newAdminAddress = accounts[2];
+
+      newInstitutionContractAddress.addNewAdmin(newAdminFirstName, newAdminSurname, newAdminAddress, { from: prospectiveAdminAccount });
     })
 
     /*
