1ff14b960024fa023e3dda43efe88ff240e6a808
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-12 10:36:17 +0100

    Institution tests

--- dapp-backend/Institiution.test.js
+++ dapp-backend/Institiution.test.js
@@ -8,16 +8,31 @@ require('chai')
 
 
 contract('Institution', accounts => {
-  beforeEach(async function () {
-    this.institution = await Institution.new()
+  // UniversityVoting contract is responsible for deploying Institution, so mimick this flow in tests.
+  let universityVoting;
+  let institution;
+  // Me, as the owner and deployer of the contract.
+   const developerAccount = accounts[0];
+  // Account for admin who makes a request for a new Institution
+  const prospectiveAdminAccount = accounts[1];
 
-  })
+  const institutionName = "Ulster University";
+  const adminFirstName = "John";
+  const adminSurname = "Francis";
 
-  describe('election contract creation', function () {
+  describe('deploy and use the child institution contract', function () {
+    before(async function() {
+      universityVoting = await UniversityVoting.new({ from: developerAccount });
+    });
+    after(async function() {
+      await universityVoting.kill();
+    });
+
     it('creates a new election', async function () {
     const result = await this.institution.createElection()
     })
 
+    /*
     it('stores the election contract address', async function () {
         const result = await this.institution.createElection()
         // Get emitted event
@@ -28,7 +43,7 @@ contract('Institution', accounts => {
         // election.
         const createdElection = await this.institution.elections(0);
         createdElection.should.equal(newContractAddress);
-        })
+        }) */
 
   })
 
