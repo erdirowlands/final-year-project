258a1c8370a6af06f73355393acab724c316dcca
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-12 11:51:07 +0100

    Change name of result to transaction receipt in inst test

--- dapp-backend/test/Institiution.test.js
+++ dapp-backend/test/Institiution.test.js
@@ -36,12 +36,12 @@ contract('Institution', accounts => {
         { from: prospectiveAdminAccount }
       );
       // Developer Approves the request and UniversityVoting contract the new Institution contract.
-      const result = await universityVoting.approveInstitutionCreation(
+      const transactionReceipt = await universityVoting.approveInstitutionCreation(
         prospectiveAdminAccount,
         { from: developerAccount }
       );
       // Get emitted event from initialiseInstitutionWithAdmin()
-      const log = await result.logs[0].args;
+      const log = await transactionReceipt.logs[0].args;
       // Get newly created contract address from event and use truffle-contract to get an instance.
       newInstitutionContractAddress = await Institution.at(log.institution);
     });
@@ -54,11 +54,11 @@ contract('Institution', accounts => {
       const newAdminSurname = "Sisko"
       const newAdminAddress = accounts[2];
 
-      const result = await newInstitutionContractAddress.addNewAdmin(newAdminFirstName, newAdminSurname, newAdminAddress, { from: prospectiveAdminAccount });
-      truffleAssert.eventEmitted(result, "LogNewAdmin", (event) => {
+      const transactionReceipt = await newInstitutionContractAddress.addNewAdmin(newAdminFirstName, newAdminSurname, newAdminAddress, { from: prospectiveAdminAccount });
+      truffleAssert.eventEmitted(transactionReceipt, "LogNewAdmin", (event) => {
         return newAdminAddress.should.equal(event.newAdmin);
     });
-    //  const log = await result.logs[0].args;
+    //  const log = await transactionReceipt.logs[0].args;
       
     //  newAdminAddress.should.equal(log.adminAddress);
     })
@@ -66,9 +66,9 @@ contract('Institution', accounts => {
 
     /*
     it('stores the election contract address', async function () {
-        const result = await this.institution.createElection()
+        const transactionReceipt = await this.institution.createElection()
         // Get emitted event
-        const log = await result.logs[0].args;
+        const log = await transactionReceipt.logs[0].args;
         // Get newly created contract address from event
         const newContractAddress = await log.election;
         // Check if the address stored in the elections array matches the newly created 
