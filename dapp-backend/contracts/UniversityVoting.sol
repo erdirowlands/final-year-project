a0f643e3fedada1bb1c15dd6c5de689d9f2d17ba
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-13 20:59:05 +0100

    Delete bytes32 array method

--- dapp-backend/contracts/UniversityVoting.sol
+++ dapp-backend/contracts/UniversityVoting.sol
@@ -112,24 +112,6 @@ contract UniversityVoting is Ownable, ApprovalQueue {
         return string(bytesStringTrimmed);
     }
 
-    function bytes32ArrayToString (bytes32[] memory data) public returns (string memory) {
-        bytes memory bytesString = new bytes(data.length * 32);
-        uint urlLength;
-        for (uint i = 0; i < data.length; i++) {
-            for (uint j = 0; j < 32; j++) {
-                byte char = byte(bytes32(uint(data[i]) * 2 ** (8 * j)));
-                if (char != 0) {
-                    bytesString[urlLength] = char;
-                    urlLength += 1;
-                }
-            }
-        }
-        bytes memory bytesStringTrimmed = new bytes(urlLength);
-        for (uint i = 0; i < urlLength; i++) {
-            bytesStringTrimmed[i] = bytesString[i];
-        }
-        return string(bytesStringTrimmed);
-    }
 /*
     function approveInstitutionCreation(address adminAddress) public onlyOwner {
         require(isApprovalStored(adminAddress), "Approval not found");
