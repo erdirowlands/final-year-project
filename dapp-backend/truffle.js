5daed015af4606adf156386e344b8a889a24460d
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-17 00:04:12 +0100

    Fix url

--- dapp-backend/truffle.js
+++ dapp-backend/truffle.js
@@ -29,7 +29,7 @@ module.exports = {
 
     rinkeby: {
       provider: function() {
-        return new HDWalletProvider(process.env.DAPP_PRIVATE_KEY, "https://rinkeby.infura.io/v3/82bc3d3749d049248ee3333c6efabc25");
+        return new HDWalletProvider("5D0A44B2F735738D8D121CF8866D45A516582C5DCFACD05E79F431FD3BBE1B98", "rinkeby.infura.io/v3/f2c4ebd8ed604600a20a3236bffb51df");
       },
       network_id: '4',
     },
