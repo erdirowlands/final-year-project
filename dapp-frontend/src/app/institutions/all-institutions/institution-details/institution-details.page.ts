c2b985da08774abb802d3ee96469809c9c6be2a2
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-18 22:35:15 +0100

    Add inst details

---
+++ dapp-frontend/src/app/institutions/all-institutions/institution-details/institution-details.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-institution-details',
+  templateUrl: './institution-details.page.html',
+  styleUrls: ['./institution-details.page.scss'],
+})
+export class InstitutionDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- dapp-frontend/src/app/institutions/institutions-routing.module.ts
+++ dapp-frontend/src/app/institutions/institutions-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { InstitutionsPage } from './institutions.page';
+
+const routes: Routes = [
+  {
+    path: 'institutions-tabs',
+    component: InstitutionsPage,
+    children: [
+      {
+        path: ' all-instiutions',
+        children: [
+          {
+            path: '',
+            loadChildren:
+              './all-institutions/all-institutions.module#AllInstitutionsPageModule'
+          }
+          {
+              path: ''
+          }
+        ]
+      }
+    ]
+  },
+  { path: 'institution-details', loadChildren: './all-institutions/institution-details/institution-details.module#InstitutionDetailsPageModule' }
+];
 
 @NgModule({
-    imports: [RouterModule]
+  imports: [RouterModule.forChild()],
+  exports: [RouterModule]
 })
-
 export class InstitutionsRoutingModule {}
