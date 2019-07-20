371d71c961e7f4813338838f82704720bd0770b2
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-20 18:08:05 +0100

    Requireartifact

--- dapp-frontend/src/app/blockchain/contracts/institution-contract.service.ts
+++ dapp-frontend/src/app/blockchain/contracts/institution-contract.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Web3ProviderService } from '../provider/web3provider.service';
 
+import { contract } from 'truffle-contract';
+const institution_artifact = require('./contracts/artifacts/Institution.json');
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class InstitutionContractService {
 
+  public Institution: any;
+
   constructor(private web3Provider: Web3ProviderService) { }
+
+  private initialiseInstitutionContract() {
+    this.Institution = contract(metacoin_artifacts);
+    this.Institution.setProvider(this.web3Provider.getWeb3);
+  }
+
 }
