72352813ef6c1855acdb168cb7521363fce9bc5d
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-20 18:17:38 +0100

    UV service

--- dapp-frontend/src/app/blockchain/contracts/university-voting/university-voting.service.ts
+++ dapp-frontend/src/app/blockchain/contracts/university-voting/university-voting.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Web3ProviderService } from '../../provider/web3provider.service';
+
+import { contract } from 'truffle-contract';
+const universityVotingArtifact = require('../artifacts/UniversityVoting.json');
 
 @Injectable({
   providedIn: 'root'
 })
 export class UniversityVotingService {
 
-  constructor() { }
+  public Institution: any;
+
+  constructor(private web3Provider: Web3ProviderService) { }
+
+  private initialiseUniversityVotingContract() {
+    this.Institution = contract(universityVotingArtifact);
+    this.Institution.setProvider(this.web3Provider.getWeb3);
+  }
+
 }
