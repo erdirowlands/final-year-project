a0f4fa51db0cdd1b9765261ce57ed5cf915610d8
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-21 12:48:50 +0100

    inst approval request dev

--- dapp-frontend/src/app/owner/owner.page.ts
+++ dapp-frontend/src/app/owner/owner.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UniversityVotingService } from 'src/app/blockchain/contracts/university-voting/university-voting.service';
+import { WalletService } from 'src/app/blockchain/wallet/wallet.service';
+
 
 @Component({
   selector: 'app-owner',
