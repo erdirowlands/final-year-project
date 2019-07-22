1ec02337c71821f8174075fa3dcefea59be826db
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-22 21:44:55 +0100

    Update

--- dapp-frontend/src/app/blockchain/provider/web3provider.service.ts
+++ dapp-frontend/src/app/blockchain/provider/web3provider.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import Web3 from 'web3';
+import { ethers } from 'ethers';
+const Tx = require('ethereumjs-tx').Transaction;
+
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -26,53 +29,31 @@ export class Web3ProviderService {
     console.log(this.web3.eth.getAccounts());
   }
 
-  public getWeb3() {
-    return this.web3;
-  }
 
-  public offlineTransactionSigner() {
-    const web33 = this.web3.getWeb3();
 
-    const myContract = new web33.eth.Contract(
-      universityVotingArtifact.abi,
-      environment.ethereum.universityVotingContractAddress
+  public submitInstitutionRequestSigner(artifact: any, artifactAddress: string, requestData: string[],  walletKey: string) {
+
+    const web3Contract = this.web3;
+    const myContract = new web3Contract.eth.Contract(
+      artifact.abi,
+      artifactAddress
     );
 
-    const institutionRequest = new InstitutionApprovalRequest(
-      institutionName,
-      adminName
-    );
-    const requestArray = [
-      institutionRequest.adminName,
-      institutionRequest.institutionName
-    ];
-
-    const newRequestDataAsBytes32 = requestArray.map(requestArray =>
+    const newRequestDataAsBytes32 = requestData.map(requestArray =>
       ethers.utils.formatBytes32String(requestArray)
     );
 
-    console.log(newRequestDataAsBytes32);
+    let method1 = myContract.methods.submitInstitutionApprovalRequest(newRequestDataAsBytes32).encodeABI();
 
-    // let method1 = myContract.methods.submitInstitutionApprovalRequest(newRequestDataAsBytes32).encodeABI();
-    //  let method1 = myContract.methods.submitInstitutionApprovalRequest("[0x6173647300000000000000000000000000000000000000000000000000000000, 0x6869000000000000000000000000000000000000000000000000000000000000]").encodeABI();
-    //  let method1 = myContract.methods.submitInstitutionApprovalRequest(['0x6173647300000000000000000000000000000000000000000000000000000000', '0x6869000000000000000000000000000000000000000000000000000000000000']).encodeABI();
+    
     const method1 = myContract.methods
       .approveInstitutionRequest('0xeCDED0f569Ccd0FcEF2bc359e6F742BA1d6e533A')
       .encodeABI();
-    // let method1 = myContract.methods.submitInstitutionApprovalRequest(newRequestDataAsBytes32).encodeABI();
-
-    const method1Hex = asciiToHex(method1);
-
-    // const privateKey = new Buffer('0x83bd955d4e7bad3b941f4c438ff0b05546b4d1dbc1ff2e3b276dcc70fdd36eec', 'hex');
 
     const privateKey = Buffer.from(
       '5D0A44B2F735738D8D121CF8866D45A516582C5DCFACD05E79F431FD3BBE1B98',
       'hex'
     );
-
-    // let pk = web33.eth.accounts.privateKeyToAccount('0x5D0A44B2F735738D8D121CF8866D45A516582C5DCFACD05E79F431FD3BBE1B98');
-
-    //
     const gasCost = await web33.eth.gasPrice;
 
     const theNonce = await web33.eth.getTransactionCount(
@@ -86,8 +67,8 @@ export class Web3ProviderService {
     //  const walletAccount = this.wallet.wallet[2];
 
     const rawTx = {
-      nonce: web33.utils.toHex(theNonce),
-      gasPrice: web33.utils.toHex(toWei('2', 'gwei')),
+      nonce: this.web3.utils.toHex(theNonce),
+      gasPrice: this.web3.utils.toHex(toWei('2', 'gwei')),
       gasLimit: web33.utils.toHex('5000000'),
       to: environment.ethereum.universityVotingContractAddress,
       value: '0x0',
@@ -123,5 +104,9 @@ export class Web3ProviderService {
         sanitisedError = errorString;
         break;
     }
+  }
+
+  public getWeb3() {
+    return this.web3;
   }
 }
