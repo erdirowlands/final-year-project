f8eaa68fb1164db839777eb4febb6b06dc160f0a
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-22 17:59:26 +0100

    Backup

--- dapp-frontend/src/app/institutions/institution-approval-request/institution-approval-request.page.ts
+++ dapp-frontend/src/app/institutions/institution-approval-request/institution-approval-request.page.ts
@@ -9,11 +9,13 @@ import { environment } from 'src/environments/environment';
 
 import { InstitutionApprovalRequest } from './institution-approval-request.model';
 import { Web3ProviderService } from 'src/app/blockchain/provider/web3provider.service';
-
+const Tx = require('ethereumjs-tx').Transaction;
 
 var universityVotingArtifact = require('src/app/blockchain/contracts/artifacts/UniversityVoting.json');
 
 const { asciiToHex } = require('web3-utils');
+const { toWei } = require('web3-utils');
+const { toHex } = require('web3-utils');
 
 @Component({
   selector: 'app-institution-approval-request',
@@ -114,7 +116,7 @@ export class InstitutionApprovalRequestPage implements OnInit {
         }
       });
   } */
-
+/*
   private async  submitInstitutionApproval(institutionName: string, adminName: string) {
 
     let web33 = this.web3.getWeb3();
@@ -148,15 +150,17 @@ export class InstitutionApprovalRequestPage implements OnInit {
    // let pk = web33.eth.accounts.privateKeyToAccount('0x5D0A44B2F735738D8D121CF8866D45A516582C5DCFACD05E79F431FD3BBE1B98');
 
   // 
+    let gasCost = await  web33.eth.gasPrice;
 
-  let gasCost = await  web33.eth.gasPrice;
-
+    let theNonce = await web33.eth.getTransactionCount('0xeCDED0f569Ccd0FcEF2bc359e6F742BA1d6e533A');
+    
    // const walletAccount = this.wallet.wallet[0];
 
    // this.wallet.wallet.add(pk); 
  //  const walletAccount = this.wallet.wallet[2];
 
     let tx = {
+      nonse: theNonce,
       chainId: '4',
       to : environment.ethereum.universityVotingContractAddress,
       data : method1,
@@ -198,7 +202,102 @@ export class InstitutionApprovalRequestPage implements OnInit {
           this.showDeniedAlert(sanitisedError);
         }
       });
-  }
+  } */
+
+  private async  submitInstitutionApproval(institutionName: string, adminName: string) {
+
+    let web33 = this.web3.getWeb3();
+
+    let myContract = new web33.eth.Contract(universityVotingArtifact.abi, '0x7ea7CD0F22c7AdA7E7E39e9f35baD5D732b914E9');
+
+    const institutionRequest = new InstitutionApprovalRequest(
+      institutionName,
+      adminName
+    );
+    const requestArray = [
+      institutionRequest.adminName,
+      institutionRequest.institutionName
+    ];
+
+    const newRequestDataAsBytes32 = requestArray.map(requestArray =>
+      asciiToHex(requestArray)
+    );
+
+    console.log(newRequestDataAsBytes32);
+
+  //let method1 = myContract.methods.submitInstitutionApprovalRequest(newRequestDataAsBytes32).encodeABI();
+  //  let method1 = myContract.methods.submitInstitutionApprovalRequest("[0x6173647300000000000000000000000000000000000000000000000000000000, 0x6869000000000000000000000000000000000000000000000000000000000000]").encodeABI();
+   let method1 = myContract.methods.submitInstitutionApprovalRequest(['0x6173647300000000000000000000000000000000000000000000000000000000', '0x6869000000000000000000000000000000000000000000000000000000000000']).encodeABI();
+
+
+    let method1Hex = asciiToHex(method1);
+
+   // const privateKey = new Buffer('0x83bd955d4e7bad3b941f4c438ff0b05546b4d1dbc1ff2e3b276dcc70fdd36eec', 'hex');
+
+   const privateKey = Buffer.from(
+    '5D0A44B2F735738D8D121CF8866D45A516582C5DCFACD05E79F431FD3BBE1B98',
+    'hex',
+  );
+
+   // let pk = web33.eth.accounts.privateKeyToAccount('0x5D0A44B2F735738D8D121CF8866D45A516582C5DCFACD05E79F431FD3BBE1B98');
+
+  // 
+    let gasCost = await  web33.eth.gasPrice;
+
+    let theNonce = await web33.eth.getTransactionCount('0xeCDED0f569Ccd0FcEF2bc359e6F742BA1d6e533A');
+    
+   // const walletAccount = this.wallet.wallet[0];
+
+   // this.wallet.wallet.add(pk); 
+ //  const walletAccount = this.wallet.wallet[2];
+
+    let rawTx = {
+      nonce: toHex(theNonce),
+      gasPrice: toHex(gasCost),
+      gas: toHex('6700000'),
+      to : environment.ethereum.universityVotingContractAddress,
+      value: toHex(toWei('0','ether')),
+      data : method1,
+  };
+  const tx = new Tx(rawTx);
+  tx.sign(privateKey);
+
+  const serializedTx = tx.serialize();
+
+    this.loadingCtrl
+      .create({ keyboardClose: true, message: 'Logging in...' })
+      .then(async loadingEl => {
+        try {
+          loadingEl.present();
+          // Institution data
+            web33.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('receipt', console.log);
+         // console.log(result);
+          loadingEl.dismiss();
+          this.router.navigate(['/institutions/tabs/view']);
+          this.showSucessfulAlert();
+        } catch (err) { 
+        //  console.log(err);
+          const errorString = err.toString();
+          let sanitisedError; 
+          switch (errorString) { 
+            // tslint:disable-next-line: max-line-length
+            case 'Error: Returned error: VM Exception while processing transaction: revert You have an outstanding request, please wait for that to be processed -- Reason given: You have an outstanding request, please wait for that to be processed.':
+              sanitisedError =
+                'You have an outstanding request, please wait for that to be processed';
+              break;
+            case 'Error: Returned error: VM Exception while processing transaction: revert This approval has already been submitted!':
+              sanitisedError = 'This approval has already been submitted!';
+              break;
+            default:
+              sanitisedError = errorString;
+              break;
+          } 
+          loadingEl.dismiss();
+          this.router.navigate(['/institutions/tabs/view']);
+          this.showDeniedAlert(sanitisedError);
+        }
+      });
+    }
 
   private showDeniedAlert(message: string) {
     this.alertCtrl
