857b1831e2b379d1f79f832b382435420130f279
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-22 22:13:29 +0100

    move signer to web3 provider

--- dapp-frontend/src/app/blockchain/contracts/university-voting/university-voting.service.ts
+++ dapp-frontend/src/app/blockchain/contracts/university-voting/university-voting.service.ts
@@ -1,39 +1,110 @@
 import { Injectable } from '@angular/core';
 import { Web3ProviderService } from '../../provider/web3provider.service';
+import { ethers } from 'ethers';
+const Tx = require('ethereumjs-tx').Transaction;
 
 import { environment } from 'src/environments/environment';
 
-
-
-
 declare var require: any;
 
-var universityVotingArtifact = require('../artifacts/UniversityVoting.json');
+let universityVotingArtifact = require('../artifacts/UniversityVoting.json');
 
 @Injectable({
   providedIn: 'root'
 })
 export class UniversityVotingService {
-
   private _universityVotingAbstraction: any;
+  private web3;
 
-  constructor(private web3Provider: Web3ProviderService) { 
+  constructor(private web3Provider: Web3ProviderService) {
     this.generateContractAbstraction();
+    this.web3 = this.web3Provider.getWeb3;
   }
 
-  private async  generateContractAbstraction() {
+  private async generateContractAbstraction() {
     const web3 = this.web3Provider.getWeb3();
-    this._universityVotingAbstraction = new web3.eth.Contract(universityVotingArtifact.abi, environment.ethereum.universityVotingContractAddress);
+    this._universityVotingAbstraction = new web3.eth.Contract(
+      universityVotingArtifact.abi,
+      environment.ethereum.universityVotingContractAddress
+    );
   }
-
-
 
   public get universityVotingAbstraction(): any {
     return this._universityVotingAbstraction;
   }
 
- 
+  public async submitInstitutionRequestSigner(
+    artifact: any,
+    artifactAddress: string,
+    submittingAddress: string,
+    requestData: string[],
+    walletKey: string
+  ) {
+    const web3Contract = this.web3;
+    const myContract = new web3Contract.eth.Contract(
+      artifact.abi,
+      artifactAddress
+    );
 
+    const newRequestDataAsBytes32 = requestData.map(requestArray =>
+      ethers.utils.formatBytes32String(requestArray)
+    );
 
+    const submitInstitutionContractMethod = myContract.methods
+      .submitInstitutionApprovalRequest(newRequestDataAsBytes32)
+      .encodeABI();
+
+    const gasCost = await this.web3.eth.gasPrice;
+    const currentNonce = await this.web3.eth.getTransactionCount(
+      '0xeCDED0f569Ccd0FcEF2bc359e6F742BA1d6e533A',
+      'pending'
+    );
+
+    // Construct the raw transaction.
+    const rawTx = {
+      nonce: this.web3.utils.toHex(currentNonce),
+      gasPrice: this.web3.utils.toHex(this.web3.utils.toWei('2', 'gwei')),
+      gasLimit: this.web3.utils.toHex('5000000'),
+      to: artifactAddress,
+      value: '0x0',
+      data: submitInstitutionContractMethod
+    };
+
+    // Sign the raw transaction.
+    const tx = new Tx(rawTx, { chain: 'kovan', hardfork: 'petersburg' });
+    const privateKey = Buffer.from(walletKey, 'hex');
+    tx.sign(privateKey);
+    const serializedTx = tx.serialize();
+
+    // Now we want to send the raw transaction that has been signed with
+    // the user's private key.
+    try {
+      this.web3.eth
+        .sendSignedTransaction('0x' + serializedTx.toString('hex'))
+        .on('receipt', console.log)
+        .sendSignedTransaction('0x' + serializedTx.toString('hex'))
+        .on('transactionHash', console.log)
+        .on('receipt', console.log)
+        .on('confirmation', console.log)
+        .on('error', console.log);
+    } catch (err) {
+      console.log(err);
+      const errorString = err.toString();
+      let sanitisedError;
+      switch (errorString) {
+        // tslint:disable-next-line: max-line-length
+        case 'Error: Returned error: VM Exception while processing transaction: revert You have an outstanding request, please wait for that to be processed -- Reason given: You have an outstanding request, please wait for that to be processed.':
+          sanitisedError =
+            'You have an outstanding request, please wait for that to be processed';
+          break;
+        case 'Error: Returned error: VM Exception while processing transaction: revert This approval has already been submitted!':
+          sanitisedError = 'This approval has already been submitted!';
+          break;
+        default:
+          sanitisedError = errorString;
+          break;
+      }
+    }
+  }
 
 }
