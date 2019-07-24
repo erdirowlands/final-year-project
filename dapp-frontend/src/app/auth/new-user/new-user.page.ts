16a1cf6f2cd57bcdccd561e78cd6690291540232
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-24 19:44:14 +0100

    Image

--- dapp-frontend/src/app/auth/new-user/new-user.page.ts
+++ dapp-frontend/src/app/auth/new-user/new-user.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { NavParams,  ModalController} from '@ionic/angular';
 
 
-
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.page.html',
   styleUrls: ['./new-user.page.scss'],
 })
 export class NewUserPage implements OnInit {
+  
+  placeHolderImage = './assets/select-institutions/institution_item.png';
 
   constructor(private navParams: NavParams, private modalCtrl: ModalController) { }
 
