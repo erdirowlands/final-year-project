282e05faa7528b06ae3d6bc06c0c8815c51bfc35
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-24 15:26:31 +0100

    Changedd spinner load logic - make sure this works well

--- dapp-frontend/src/app/blockchain/wallet/wallet.service.ts
+++ dapp-frontend/src/app/blockchain/wallet/wallet.service.ts
@@ -36,7 +36,7 @@ export class WalletService {
   constructor(private web3ProviderService: Web3ProviderService) {
     this._web3Instance = this.web3ProviderService.getWeb3();
     this._wallet = this._web3Instance.eth.accounts.wallet;
-    this.getKeyPair("password"); // TODO FOR DEV SO I CAN LOGIN AUTOMAGICALLY
+   // this.getKeyPair("password"); // TODO FOR DEV SO I CAN LOGIN AUTOMAGICALLY
 
   }
 
