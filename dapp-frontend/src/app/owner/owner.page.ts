a85969be4ae14da4901edf6d9d543ba1ea0d3d25
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-25 02:51:20 +0100

    dev

--- dapp-frontend/src/app/owner/owner.page.ts
+++ dapp-frontend/src/app/owner/owner.page.ts
@@ -5,7 +5,7 @@ import { UniversityVotingService } from 'src/app/blockchain/contracts/university
 import { WalletService } from 'src/app/blockchain/wallet/wallet.service';
 import { Web3ProviderService } from 'src/app/blockchain/provider/web3provider.service';
 
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-owner',
@@ -37,7 +37,46 @@ export class OwnerPage implements OnInit {
   }
 
   async approveRequest(submittingAddress: string) { 
-    
+    {
+      this.loadingCtrl
+        .create({ keyboardClose: true, message: 'Sending request to Ethereum...' })
+        .then(async loadingEl => {
+          try {
+            loadingEl.present();
+            await this.universityVotingContract.approveInstitutionRequestSigner(
+              submittingAddress
+            );
+            loadingEl.dismiss();
+            this.router.navigate(['/institutions/tabs/view']);
+            this.showSucessfulAlert();
+          } catch (err) {
+            console.log(err);
+            const errorString = err.toString();
+            let sanitisedError;
+            switch (errorString) {
+              // tslint:disable-next-line: max-line-length
+                          
+              case 'Error: Transaction ran out of gas. Please provide more gas:':
+                sanitisedError =
+                  'You have an outstanding request, please wait for that to be processed';
+                break;
+              case 'Error: Returned error: VM Exception while processing transaction: revert You have an outstanding request, please wait for that to be processed -- Reason given: You have an outstanding request, please wait for that to be processed.':
+                sanitisedError =
+                  'You have an outstanding request, please wait for that to be processed';
+                break;
+              case 'Error: Returned error: VM Exception while processing transaction: revert This approval has already been submitted!':
+                sanitisedError = 'This approval has already been submitted!';
+                break;
+              default:
+                sanitisedError = errorString;
+                break;
+            }
+            loadingEl.dismiss();
+            this.router.navigate(['/institutions/tabs/view']);
+            this.showDeniedAlert(sanitisedError);
+          }
+        });
+    }
   }
 
   async getApprovalRequests() {
@@ -77,4 +116,26 @@ export class OwnerPage implements OnInit {
       console.log('Refresh: event');
     });
   }
+
+
+  private showDeniedAlert(message: string) {
+    this.alertCtrl
+      .create({
+        header: 'New institution request denied',
+        message,
+        buttons: ['Okay']
+      })
+      .then(alertEl => alertEl.present());
+  }
+
+  private showSucessfulAlert() {
+    this.alertCtrl
+      .create({
+        header:
+          'Request submitted. We just need to verify your university status. Please check back later.',
+        buttons: ['Okay']
+      })
+      .then(alertEl => alertEl.present());
+  }
+
 }
