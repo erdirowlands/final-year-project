968ff20d84747e17cb0383430eb297a9bd40126d
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-26 13:09:11 +0100

    remove timed crowdsale from thing

--- dapp-backend/contracts/Institution.sol
+++ dapp-backend/contracts/Institution.sol
@@ -167,8 +167,8 @@ contract Institution is ApprovalQueue {
     event NewElectionCreated(address election);
     /**
     Create a new Election contract which can then be configured by a customer per their requirements. */
-    function createElection(uint256 openingTime, uint256  closingTime, string memory description) public isAdmin(msg.sender) isAuthorisedAdmin(msg.sender) {
-        _tokenAuthorisation = new VotingTokenAuthorisation(Institution(this), msg.sender, openingTime, closingTime, _deployedVotingToken);
+    function createElection(uint256 openingTime, uint256  closingTime, string memory description) public  {
+        _tokenAuthorisation = new VotingTokenAuthorisation(Institution(this), msg.sender,  _deployedVotingToken);
         // Let VotingTokenAuthorisation have the role as minter so it can mint tokens for voters upon request.
         _deployedVotingToken.addMinter(address(_tokenAuthorisation));
         // Create new Election contract.
--- dapp-backend/truffle.js
+++ dapp-backend/truffle.js
@@ -33,7 +33,6 @@ module.exports = {
       provider: function() {
         return new HDWalletProvider("5D0A44B2F735738D8D121CF8866D45A516582C5DCFACD05E79F431FD3BBE1B98", "https://rinkeby.infura.io/v3/f2c4ebd8ed604600a20a3236bffb51df");
       },
-      gasPrice: 50000000000,
       network_id: '4',
     },
 
