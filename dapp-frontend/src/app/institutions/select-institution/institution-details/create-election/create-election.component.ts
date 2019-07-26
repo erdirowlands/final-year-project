916836629a36bcddfad7a0df6d3af0bede8e2ae5
Erdi Rowlands <erdirowlands@gmail.com>
2019-07-26 04:55:25 +0100

    create election

--- dapp-frontend/src/app/institutions/select-institution/institution-details/create-election/create-election.component.ts
+++ dapp-frontend/src/app/institutions/select-institution/institution-details/create-election/create-election.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import {
+  ModalController,
+  LoadingController,
+  AlertController
+} from '@ionic/angular';
+import { NgForm } from '@angular/forms';
+
+import { InstitutionContractService } from 'src/app/blockchain/contracts/institution-contract/institution-contract.service';
+import { WalletService } from 'src/app/blockchain/wallet/wallet.service';
 
 @Component({
   selector: 'app-create-election',
   templateUrl: './create-election.component.html',
-  styleUrls: ['./create-election.component.scss'],
+  styleUrls: ['./create-election.component.scss']
 })
 export class CreateElectionComponent implements OnInit {
-
   @Input() institutionAddress: string;
-  constructor() { }
+  @ViewChild('f') form: NgForm;
+  constructor(
+    private modalCtrl: ModalController,
+    private institutionContract: InstitutionContractService,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {}
 
+  onCreateElection() {
+    if (!this.form.valid) {
+      return;
+    }
+  }
+
+  onCancel() {
+    this.modalCtrl.dismiss(null, 'cancel');
+  }
 }
